8ddc6bb7829ecc57e08ed627576436fd99de26ce
jaogrs <jgabrielrodrigues2@gmail.com>
2022-05-30 17:28:12 -0300

    refatorcao conect

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,11 +1,12 @@
 const conect = require('../db/storeManager');
 
-const getAll = () => conect.execute('select * from products');
+const getAll = () => conect.execute('select * from StoreManager.products');
 
-const getById = (id) => conect.execute('select * from products where products.id = ?', [id]);
+const getById = (id) => conect.execute(`select * from StoreManager.products
+where products.id = ?`, [id]);
 
-const add = ({ name, quantity }) => conect.execute(`INSERT INTO products (name, quantity)
-VALUES(?, ?)`,
+const add = ({ name, quantity }) => conect.execute(`INSERT INTO StoreManager.products
+(name, quantity) VALUES(?, ?)`,
  [name, quantity]);
 
 module.exports = { getAll, getById, add };
--- models/salesModel.js
+++ models/salesModel.js
@@ -1,10 +1,11 @@
 const connection = require('../db/storeManager');
 
 const getAll = () => connection.execute(`SELECT sp.sale_id as saleId, s.date, sp.product_id as
-productId, sp.quantity FROM sales_products sp INNER JOIN sales s ON s.id = sp.sale_id;`);
+productId, sp.quantity FROM StoreManager.sales_products sp INNER JOIN
+sales s ON s.id = sp.sale_id;`);
 
 const getById = (id) => connection.execute(`SELECT s.date, sp.product_id
-as productId, sp.quantity FROM sales_products sp INNER JOIN sales s ON s.id = sp.sale_id
-WHERE sp.sale_id = ?`, [id]);
+as productId, sp.quantity FROM StoreManager.sales_products sp INNER JOIN sales s ON s.id =
+sp.sale_id WHERE sp.sale_id = ?`, [id]);
 
 module.exports = { getAll, getById };
