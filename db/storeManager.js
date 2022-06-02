9ea2a40e6f46c4b13adeb5342882c40450700f20
jaogrs <jgabrielrodrigues2@gmail.com>
2022-06-02 18:05:30 -0300

    requisito 1 teste 2

--- db/storeManager.js
+++ db/storeManager.js
@@ -7,5 +7,5 @@ const connection = mysql.createPool({
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
-
+// kk
 module.exports = connection;
